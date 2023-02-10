51d0e6749e15ee4effd83baa481d2b542cc814a4
Michael <auger.michaell@gmail.com>
2023-02-10 17:11:57 +0100

    commit

--- 05_PHP_Calculator/addition.php
+++ 05_PHP_Calculator/addition.php
@@ -1,11 +1,5 @@
-<html>
-
-<body>
-    <!--Your code goes here-->
-    <?= "The sum of ${_GET['add_first']} and ${_GET['add_second']} is:" ?>
-    <h4><?= $_GET['add_first'] + $_GET['add_second'] ?></h4>
-
-    <a href="index.php">Reset</a>
-</body>
-
-</html> 
+<?PHP
+$verb = "tutu";
+$noun = "ff";
+$adjective = "fff";
+echo "\nI have always been obsessed with {$noun}s. I'm {$adjective}ish. I'm always {$verb}ing.";
